f0cab5fbf0594d69fedf07da0dcc051d01948a8a
Diego <skdiegomxtr@gmail.com>
2018-03-27 14:41:42 -0500

    Estoy en formulario memoria

--- vista/formulario/caracteristicas_pantallaform.php
+++ vista/formulario/caracteristicas_pantallaform.php
@@ -1,12 +1,10 @@
-<form action = "" method="POST" id="a">
-			<input type="text" name="pulgadas" value="" placeholder="pulgadas" />
+<input type="text" name="pulgadas" value="" placeholder="pulgadas" />
+<select id="tipo_pantalla" name="tipo_pantalla_id">
+<?php
+include_once substr(getcwd(), 0, 26) . '\controller\tipo_pantallaController.php';
+$var = new tipo_pantallaController();
+$var->listar();
+?>
+</select>
 
-			<input type="text" name="tipo_pantalla_id" value="" placeholder="tipo_pantalla_id" />
 
-				<input type="submit" value="Enviar" />
-				</form>
-                <script type="text/javascript" src="js/Midwife.js"></script>
-                <script type="text/javascript">
-                    cargar('caracteristicas_pantalla','crear');
-                </script>
-				
